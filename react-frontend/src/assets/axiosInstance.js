a00542339c39d99f614cf01cefafa9e3aa8296f7
Ujjwal Sharma <ujjwalsharma@Dikshants-MacBook-Air.local>
2025-07-06 03:24:18 +0530

    Request Interceptor implement

--- react-frontend/src/assets/axiosInstance.js
+++ react-frontend/src/assets/axiosInstance.js
@@ -3,6 +3,26 @@ import axios from "axios"
 const baseURL = import.meta.env.VITE_BACKEND_BASE_URL
 const axiosInstance = axios.create({
     baseURL: baseURL,
+    headers: {
+        'content-type': 'application/json',
+    }
 })
+
+// request interceptor
+axiosInstance.interceptors.request.use(
+    function(config){
+        const accessToken = localStorage.getItem('accessToken')
+        if(accessToken){
+            config.headers['Authorization'] = `Bearer ${accessToken}`
+        }
+        console.log(config)
+        return config
+},  function(error){
+    console.log(error)
+    return Promise.reject(error)
+})
+
+
+
 
 export default axiosInstance;
--- react-frontend/src/assets/components/dashboard/Dashboard.jsx
+++ react-frontend/src/assets/components/dashboard/Dashboard.jsx
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import axiosInstance from '../../axiosInstance'
 
 const Dashboard = () => {
     const [data, setData] = useState('')
-    const accessToken = localStorage.getItem('accessToken')
 
     useEffect(() => {
         const fetchProtectedData = async () => {
             try{
-                const response = await axiosInstance.get('/protected-view/', {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`
-                    }
-                })
+                const response = await axiosInstance.get('/protected-view/')
                 console.log(response)
                 setData(response.data.status)
             }catch(error){
-                console.log(error.response.data.detail)
+                console.log(error)
             }
         }
         fetchProtectedData()
