fd45c3ecdf78e56a8ec420ebafffe4a631254c92
Ujjwal Sharma <ujjwalsharma@Dikshants-MacBook-Air.local>
2025-07-16 01:40:27 +0530

    Generating plot of stock and saving using AGG Backend

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -3,10 +3,13 @@ from rest_framework.views import APIView
 from .serializers import StockPredictionSerializer
 from rest_framework.response import Response
 from rest_framework import status
+import matplotlib.pyplot as plt
 from datetime import datetime
+from django.conf import settings
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import os
 
 # Create your views here.
 
@@ -23,5 +26,25 @@ class StockPredictionAPIView(APIView):
             df = yf.download(ticker, start, end)
             if df.empty:
                 return Response({'error': 'No data found for the given ticker', 'status': status.HTTP_404_NOT_FOUND})
+            
+            df.reset_index(inplace=True)
             print(df)
+
+            # Generate Basic Plot
+            plt.switch_backend('AGG')
+            plt.figure(figsize=(12, 5))
+            plt.plot(df.Close, label='Closing Price')
+            plt.title(ticker)
+            plt.xlabel('Days')
+            plt.ylabel('Price')
+            plt.legend()
+
+            # Save Plot
+            image_name = f'{ticker}_plot.png'
+            image_path = os.path.join(settings.MEDIA_ROOT, image_name)
+            plt.savefig(image_path)
+            plt.close()
+            image_url = settings.MEDIA_URL + image_name
+            print(image_url)
+            
             return Response('Success')
