abdffc07125e999ba01d16a72c0227d92a9a88da
Ujjwal Sharma <ujjwalsharma@Dikshants-MacBook-Air.local>
2025-07-16 00:39:39 +0530

    Error handling and loading spinner

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import StockPredictionSerializer
 from rest_framework.response import Response
+from rest_framework import status
+from datetime import datetime
+import yfinance as yf
+import pandas as pd
+import numpy as np
 
 # Create your views here.
 
@@ -10,4 +15,13 @@ class StockPredictionAPIView(APIView):
         serializer = StockPredictionSerializer(data=request.data)
         if serializer.is_valid():
             ticker = serializer.validated_data['ticker']
-            return Response(f'Success : {ticker}')
+
+            # Fetch data from yFinance
+            now = datetime.now()
+            start = datetime(now.year - 10, now.month, now.day)
+            end = now
+            df = yf.download(ticker, start, end)
+            if df.empty:
+                return Response({'error': 'No data found for the given ticker', 'status': status.HTTP_404_NOT_FOUND})
+            print(df)
+            return Response('Success')
--- react-frontend/src/assets/components/dashboard/Dashboard.jsx
+++ react-frontend/src/assets/components/dashboard/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import axiosInstance from '../../axiosInstance'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'
 
 const Dashboard = () => {
     const [data, setData] = useState('')
     const [ticker, setTicker] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
 
     useEffect(() => {
         const fetchProtectedData = async () => {
@@ -20,20 +24,30 @@ const Dashboard = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        setLoading(true)
         try{
             const response = await axiosInstance.post('/predict/', {'ticker': ticker})
             console.log(response.data)
+            if(response.data.error){
+                setError(response.data.error)
+            }
         }catch(error){
+            setInvalid(true)
             console.log(error)
+        }finally{
+            setLoading(false)
         }
     }
 
   return (
     <div className="container mb-5">
         <div className="col-md-6 mx-auto">
+            <div className="mb-3">
+                {error && <div className="alert alert-danger">{error}</div> }
+            </div> 
             <form onSubmit={handleSubmit}>
                 <input className='form-control mb-3' type="text" placeholder='Select Stock Ticker' onChange={(e) => setTicker(e.target.value)} required/>
-                <button type="submit" className='btn btn-info'>See Prediction</button>
+                {!loading ? (<button type='submit' className='btn btn-info'>See Prediction</button>) : (<button type='submit' className='btn btn-info' disabled><FontAwesomeIcon icon={faSpinner} spin/>Please wait...</button>)}
             </form>
         </div>
     </div>
